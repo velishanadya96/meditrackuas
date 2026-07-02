be960904de219e39edd5a8cb6f57685da0a05f7e
velishanadya96 <velishanadya96@gmail.com>
2026-07-02 13:07:11 +0700

    newdirection

--- api/login.php
+++ api/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/api/db.php';
+require_once __DIR__ . '/db.php';
 
 // Kalau sudah login, langsung redirect sesuai role
 if (isset($_SESSION['user_id'])) {
