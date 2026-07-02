92962c292f5eff04036fd04f8600eec7f7fa5c36
velishanadya96 <velishanadya96@gmail.com>
2026-07-02 13:13:37 +0700

    db

--- api/db.php
+++ api/db.php
@@ -12,7 +12,7 @@ function getDB(): PDO
     $port = getenv('DB_PORT') ?: '4000';
     $dbname = getenv('DB_NAME') ?: 'meditrack';
     $user = getenv('DB_USER') ?: '2Ggt5JVH8GPf4fe.root';
-    $pass = getenv('DB_PASS') ?: 'Dvqi3NZvhn3tFcc0';
+    $pass = getenv('DB_PASS') ?: 'GVbegRf3k8euFcC0'; 
 
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
@@ -20,19 +20,20 @@ function getDB(): PDO
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
+        // Tambahkan opsi SSL ini agar TiDB mengizinkan koneksi dari Vercel
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ];
 
     try {
         $pdo = new PDO($dsn, $user, $pass, $options);
-
     } catch (PDOException $e) {
-
+        // Vercel kadang menangkap error PHP sebelum dicetak, 
+        // tapi ini akan membantu jika dijalankan di lokal
         die("
         <h2>Koneksi Database Gagal</h2>
         <b>Error :</b><br>
         {$e->getMessage()}
         ");
-
     }
 
     return $pdo;
